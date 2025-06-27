da8f23e10a4c0e1cbcdabe87055cd7b6c79add35
jprodriguezb <jprodriguezb@utn.ac.cr>
2025-06-27 13:54:26 -0600

    Interface List
    Se crea la interface list con el método add

---
+++ src/Lists/List.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author jprod
+ */
+public interface List <T> {
+    public boolean add(T t);
+    
+}
